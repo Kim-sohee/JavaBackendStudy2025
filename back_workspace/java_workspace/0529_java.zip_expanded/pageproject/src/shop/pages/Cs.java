cd96f66ea76bdeb91fcd0ea37efd1db94b799786
DESKTOP-V4KNUOF\a <Kim-sohee>
2025-05-29 12:41:42 +0900

    pc 오류로 인한 아카이브 파일 import

---
+++ back_workspace/java_workspace/0529_java.zip_expanded/pageproject/src/shop/pages/Cs.java
@@ -0,0 +1,17 @@
+package shop.pages;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//쇼핑몰의 메인 페이지
+public class Cs extends Page{
+
+	public Cs() {
+		super("고객센터 페이지에요");
+		setBackground(Color.ORANGE);
+	}
+}
