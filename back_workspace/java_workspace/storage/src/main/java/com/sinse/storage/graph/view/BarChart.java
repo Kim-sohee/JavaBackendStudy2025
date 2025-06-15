807fadb9d9556849c7ad16e403b8f4c4109079e9
sohee <soheekim980.naver.com>
2025-06-16 00:41:14 +0900

    차트 공부

---
+++ back_workspace/java_workspace/storage/src/main/java/com/sinse/storage/graph/view/BarChart.java
@@ -0,0 +1,118 @@
+package com.sinse.storage.graph.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+
+import javax.swing.SwingUtilities;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class BarChart extends ApplicationFrame{
+	//생성자
+	public BarChart(final String title) {
+		super(title);
+		final CategoryDataset dataset = createDataset();
+		final JFreeChart chart = createChart(dataset);
+		final ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(500, 700));
+		setContentPane(chartPanel);
+	}
+	
+	//sample dataset
+	private CategoryDataset createDataset() {
+		//row key
+		final String series1 = "First";
+		final String series2 = "Second";
+		final String series3 = "Third";
+		
+		//column key
+		final String category1 = "Category 1";
+		final String category2 = "Category 2";
+		final String category3 = "Category 3";
+		final String category4 = "Category 4";
+		final String category5 = "Category 5";
+		
+		//create the dataset
+		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		
+		dataset.addValue(1.0, series1, category1);
+		dataset.addValue(4.0, series1, category2);
+		dataset.addValue(3.0, series1, category3);
+		dataset.addValue(5.0, series1, category4);
+		dataset.addValue(5.0, series1, category5);
+		
+		dataset.addValue(5.0, series2, category1);
+		dataset.addValue(7.0, series2, category2);
+		dataset.addValue(6.0, series2, category3);
+		dataset.addValue(8.0, series2, category4);
+		dataset.addValue(4.0, series2, category5);
+		
+		dataset.addValue(4.0, series3, category1);
+		dataset.addValue(3.0, series3, category2);
+		dataset.addValue(2.0, series3, category3);
+		dataset.addValue(3.0, series3, category4);
+		dataset.addValue(6.0, series3, category5);
+		
+		return dataset;
+	}
+	
+	//create a sample chart
+	private JFreeChart createChart(final CategoryDataset dataset) {
+		//create the chart
+		final JFreeChart chart = ChartFactory.createBarChart("Bar Chart Demo", "Category", "Value",
+				dataset, PlotOrientation.VERTICAL, true, true, false);
+		
+		chart.setBackgroundPaint(Color.white);
+		
+		//get a reference to the plot for further customisation
+		final CategoryPlot plot = chart.getCategoryPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
+		
+		//set the range axis to display integers only
+		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		
+		//disable bar outlines
+		final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+		renderer.setDrawBarOutline(false);
+		
+		//set up gradient paints for series
+		final GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue,
+				0.0f, 0.0f, Color.lightGray);
+		final GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green,
+				0.0f, 0.0f, Color.lightGray);
+		final GradientPaint gp2 = new GradientPaint(0.0f, 0.0f, Color.red,
+				0.0f, 0.0f, Color.lightGray);
+		
+		renderer.setSeriesPaint(0, gp0);
+		renderer.setSeriesPaint(1, gp1);
+		renderer.setSeriesPaint(2, gp2);
+		
+		final CategoryAxis domainAxis = plot.getDomainAxis();
+		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI/6.0));
+		return chart;
+	}
+	
+	public static void main(String[] args) {
+		final BarChart demo = new BarChart("Bar Chart Demo");
+		demo.pack();
+		SwingUtilities.invokeLater(()->{
+			demo.setLocationRelativeTo(null);		//Location Center
+		});
+		demo.setVisible(true);
+	}
+}
