e53662f7b27e0ee265d9bca79c112a2d9e88ab57
Kim-sohee <qpdlql013@naver.com>
2025-06-16 15:57:06 +0900

    멀티 케스팅

---
+++ back_workspace/java_workspace/networkapp/src/main/java/com/sinse/networkapp/multicast/Client.java
@@ -0,0 +1,129 @@
+package com.sinse.networkapp.multicast;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Client extends JFrame implements Runnable{
+	JPanel p_north;
+	JComboBox cb_ip;
+	JTextField t_port;
+	JButton bt_con;
+	
+	JPanel p_center;
+	JTextArea area;
+	JScrollPane scroll;
+	JTextField t_input;
+	Thread thread;
+	Socket socket;
+	BufferedReader buffr;
+	BufferedWriter buffw;
+	
+	public Client() {
+		//생성
+		p_north = new JPanel();
+		cb_ip = new JComboBox();
+		t_port = new JTextField();
+		bt_con = new JButton("연결");
+		
+		p_center = new JPanel();
+		area = new JTextArea();
+		scroll = new JScrollPane(area);
+		t_input = new JTextField();
+		thread = new Thread(this);
+		
+		//스타일
+		p_north.setPreferredSize(new Dimension(300, 40));
+		p_north.setBackground(Color.LIGHT_GRAY);
+		cb_ip.setPreferredSize(new Dimension(100, 30));
+		t_port.setPreferredSize(new Dimension(100, 30));
+		
+		area.setPreferredSize(new Dimension(290, 400));
+		area.setBackground(Color.yellow);
+		t_input.setPreferredSize(new Dimension(290, 30));
+		
+		//조립
+		p_north.add(cb_ip);
+		p_north.add(t_port);
+		p_north.add(bt_con);
+		add(p_north, BorderLayout.NORTH);
+		
+		p_center.add(scroll);
+		p_center.add(t_input);
+		add(p_center);
+		
+		bt_con.addActionListener(e->{
+			thread.start();
+		});
+		
+		t_input.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					send();
+				}
+			}
+		});
+		
+		//이벤트
+		cb_ip.addItem("192.168.60.45");
+		cb_ip.addItem("192.168.60.32");
+		cb_ip.addItem("192.168.60.50");
+		cb_ip.addItem("192.168.60.4");
+		
+		setSize(320, 530);
+		setVisible(true);
+		
+	}
+	
+	//접속이란, 서버의 ip와 포트번호를 이용하여 소켓을 생성하는 것
+	public void connectServer() {
+		String ip = (String)cb_ip.getSelectedItem();
+		int port = Integer.parseInt(t_port.getText());
+		try {
+			socket = new Socket(ip, port);
+			buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		connectServer();
+	}
+	
+	public void send() {
+		try {
+			buffw.write(t_input.getText()+"\n");
+			buffw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Client();
+	}
+}
