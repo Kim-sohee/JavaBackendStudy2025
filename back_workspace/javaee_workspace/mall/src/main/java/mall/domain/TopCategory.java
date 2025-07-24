750534ea9781ce87851f6ed7ea2d77579cf6fbc2
Kim-sohee <qpdlql013@naver.com>
2025-07-24 16:00:53 +0900

    상품 상세 보기

--- back_workspace/javaee_workspace/mall/src/main/java/mall/domain/TopCategory.java
+++ back_workspace/javaee_workspace/mall/src/main/java/mall/domain/TopCategory.java
@@ -1,8 +1,11 @@
 package mall.domain;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -13,4 +16,8 @@ public class TopCategory {
 	@Id
 	private int topcategory_id;
 	private String top_name;
+	
+	//자식인 SubCatsegory 리스트
+	@Transient
+	private List<SubCategory> subList;		//1:多 관계이므로, 자식을 collection으로 수집, 만일 자식도 부모를 같이 참조할 경우 무한루프 조심!
 }
