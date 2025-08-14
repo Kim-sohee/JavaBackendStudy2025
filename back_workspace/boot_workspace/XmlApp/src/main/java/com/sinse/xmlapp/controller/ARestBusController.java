8c9ef1f00c4c7df78d72f224bf2aa0645d7b0e54
Kim-sohee <qpdlql013@naver.com>
2025-08-14 16:29:36 +0900

    공공데이터 활용 및 구글맵 연동

--- back_workspace/boot_workspace/XmlApp/src/main/java/com/sinse/xmlapp/controller/ARestBusController.java
+++ back_workspace/boot_workspace/XmlApp/src/main/java/com/sinse/xmlapp/controller/ARestBusController.java
@@ -1,44 +1,23 @@
 package com.sinse.xmlapp.controller;
 
+import com.sinse.xmlapp.domain.Item;
+import com.sinse.xmlapp.model.bus.BusService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.List;
 
+@Slf4j
 @RestController
 public class ARestBusController {
+    private BusService busService;
+    public ARestBusController(BusService busService) {
+        this.busService = busService;
+    }
+
     @GetMapping("/buses")
-    public String getList() throws Exception {
-        String serviceKey = "h4iQorPnhgjXhZMRZBUz4808vDIVrJjIDEmVPuci4wjjyVcU2EAVTeXk7%2FhZOVJ%2BhQGylBRKhvudL9wLeulVZQ%3D%3D";
-        StringBuilder urlBuilder = new StringBuilder("https://apis.data.go.kr/6260000/BusanBIMS/busStopList"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="+serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*정류소 명*/
-        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*정류소 번호*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/xml");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        System.out.println(sb.toString());
-        return sb.toString();
+    public List<Item> getList() throws Exception {
+        return busService.parse();
     }
 }
