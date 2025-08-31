47df408f2a35571706af7f468c441de7958c80c3
Kim-sohee <qpdlql013@naver.com>
2025-09-01 00:54:40 +0900

    cors 처리

--- back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/controller/MainController.java
+++ back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/controller/MainController.java
@@ -1,14 +1,32 @@
 package com.example.studyspringjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
 public class MainController {
     @GetMapping("/")
     public String main(){
-        return "Hello World";
+        //JWT Filter 통과 후 세션 확인
+
+        //세션 현재 사용자 아이디
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //세션 현재 사용자 role
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority grantedAuthority = iterator.next();
+        String role = grantedAuthority.getAuthority();
+
+        return "[Main] username: "+username+" role: "+role;
     }
 }
