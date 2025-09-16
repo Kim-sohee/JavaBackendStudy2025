76fbabd31c7289a7591c5e1ea34906c5c9a15bd1
Kim-sohee <qpdlql013@naver.com>
2025-09-16 12:13:52 +0900

    금일수업

--- back_workspace/boot_workspace/shop_workspace/OrderService/src/main/java/com/sinse/orderservice/controller/OrderController.java
+++ back_workspace/boot_workspace/shop_workspace/OrderService/src/main/java/com/sinse/orderservice/controller/OrderController.java
@@ -3,13 +3,27 @@ package com.sinse.orderservice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class OrderController {
+    private final RestClient restClient;
+
+    public OrderController(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
     @GetMapping("/orders")
     public ResponseEntity<?> orders() {
-        return ResponseEntity.ok(Map.of("result","주문 목록입니다"));
+        Map<String, Object> response = restClient.get()
+                .uri("http://localhost:7777/products")
+                .retrieve()
+                .body(Map.class);
+        List<String> products = (List<String>)response.get("data");
+
+        return ResponseEntity.ok(Map.of("result","상품정보 댕겨온 주문 목록", "products", products));
     }
 }
--- back_workspace/front_workspace/react_workspace/adminapp/src/App.jsx
+++ back_workspace/front_workspace/react_workspace/adminapp/src/App.jsx
@@ -2,15 +2,38 @@ import Preloader from  './components/Preloader'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import ProductList from './pages/product/ProductList'
+import ProductForm from './pages/product/ProductForm'
+
+import OrderList from './pages/order/OrderList'
 
 function App() {
 
   return (
     <div className="wrapper">
-      <Preloader/>
-      <Navbar/>
-      <Sidebar/>
-      <Footer/>
+      <BrowserRouter>
+        <Preloader/>
+        <Navbar/>
+        <Sidebar/>
+        
+        {/* 페이지 전환할 영역 */}
+        <div className='content-wrapper'>
+          <session className="content">
+            <Routes>
+              {/* 상품관련 */}
+              <Route path="product/list" element={<ProductList/>}/>
+              <Route path="product/registform" element={<ProductForm/>}/>
+
+              {/* 주문관련 */}
+              <Route path="order/list" element={<OrderList/>}/>
+
+            </Routes>
+          </session>
+        </div>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
