be306ec22aabaf44ea08a170f07197ea2c5f80ca
Kim-sohee <qpdlql013@naver.com>
2025-09-20 16:42:50 +0900

    임시 수정

--- back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/mail/EmailController.java
+++ back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/mail/EmailController.java
@@ -2,21 +2,23 @@ package com.example.studyspringjwt.mail;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/send")
 public class EmailController {
     private final EmailService emailService;
 
-    @PostMapping("/send/mail")
+    @PostMapping("/mail")
     public String sendMail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
         boolean isSent = emailService.sendEmail(to, subject, text, null);
         return isSent ? "Mail sent successfully" : "Mail not sent";
     }
 
-    @PostMapping("/send/welcome")
+    @PostMapping("/welcome")
     public String sendWelcome(@RequestParam String to) {
         String subject = "Fantry 회원가입을 환영합니다!";
         String content = "<p>회원 가입이 완료되었습니다. <br>여기에서 로그인하고 다양한 아이돌 굿즈를 거래해 보세요!</p>";
@@ -24,7 +26,7 @@ public class EmailController {
         return isSent ? "회원가입 메일이 성공적으로 발송되었습니다." : "메일 발송 실패";
     }
 
-    @PostMapping("/send/reset-password")
+    @PostMapping("/reset-password")
     public String sendPasswordResetEmail(@RequestParam String to, @RequestParam String resetLink) {
         String subject = "비밀번호 재설정 요청";
         String content = "<p>비밀번호 재설정을 위해 아래 링크를 클릭하세요.</p>";
--- back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/util/CookieUtil.java
+++ back_workspace/boot_workspace/studySpringJWT/src/main/java/com/example/studyspringjwt/util/CookieUtil.java
@@ -9,7 +9,7 @@ public class CookieUtil {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24*60*60);     //쿠키의 생명주기(Refresh 토큰의 생명과 동일)
         //cookie.setSecure(true);         //Https 통신을 진행할 경우 사용
-        //cookie.setPath("/");            //쿠키가 적용될 범위
+        cookie.setPath("/");            //쿠키가 적용될 범위
         cookie.setHttpOnly(true);
 
         return cookie;
