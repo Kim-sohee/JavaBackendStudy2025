5b2111a3954610c201ec91367745b76cbf710e0c
Kim-sohee <qpdlql013@naver.com>
2025-09-22 14:07:09 +0900

    oauth 로그인 환경설정

---
+++ back_workspace/boot_workspace/shop_workspace/MemberService/src/main/java/com/sinse/memberservice/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.sinse.memberservice.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/memberapp/oauth2/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth->oauth
+                        .defaultSuccessUrl("http://localhost:7777/oauth2/redirect", true));
+
+        return http.build();
+    }
+}
